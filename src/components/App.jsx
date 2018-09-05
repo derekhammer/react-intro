0983b3c7c558bb646bb08c118487222dd727e6d3
Derek Hammer and James Hanley <derek+james@>
2018-09-05 12:50:40 -0700

    finish weekend assignment, with styling

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,9 +2,14 @@ import React from "react";
 import Header from "./Header";
 import TicketList from "./TicketList";
 
-function App(){
+function App(props){
+  var myStyledComponentStyles = {
+    backgroundColor: 'hotpink',
+    fontFamily: 'sans-serif',
+    paddingTop: '50px'
+  }
     return (
-      <div>
+      <div style={myStyledComponentStyles}>
         <Header/>
         <TicketList/>
     </div>
